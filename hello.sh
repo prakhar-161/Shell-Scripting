611d3c248616dd3fd813d615fe94290db33db3eb
Prakhar Dhama <pdhama161@gmail.com>
2021-10-16 11:32:54 +0530

    initial commit

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# 1ST VIDEO : 
+# this is a comment
+# echo "Hello world"
+
+# echo $BASH          # returns name of bash shell
+# echo $BASH_VERSION
+# echo $HOME
+# echo $PWD
+
+name=Mark
+age=10
+echo The name is $name and age is $age
