8038bb85625767fdde4c3daee9e736d6127871cd
Prakhar Dhama <pdhama161@gmail.com>
2021-10-16 17:13:19 +0530

    done arithmetic ops

---
+++ arithmeticOps.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "Enter numbers : "
+read num1 num2
+echo "numbers are : $num1 and $num2"
+
+# Method - 1
+# echo $(( num1 + num2 ))
+# echo $(( num1 - num2 ))
+# echo $(( num1 * num2 ))
+# echo $(( num1 / num2 ))
+# echo $(( num1 % num2 ))
+
+# ------------------------------------------------
+
+# Method - 2
+echo $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+echo $(expr $num1 \* $num2 )
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
+
