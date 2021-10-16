e22e63849f4da557e8e59a076fa622bb74a87c3d
Prakhar Dhama <pdhama161@gmail.com>
2021-10-16 11:37:29 +0530

    Taking user inputs

---
+++ inputs.sh
@@ -0,0 +1,24 @@
+# Taking user inputs
+
+echo "Enter name : "
+read name1 name2 name3
+echo "Entered names are : $name1, $name2, $name3"
+
+# -p allows to read input on same line
+# -sp for hidden string(for eg- passwords)
+# -a for saving inputs inside array
+
+read -p 'username : ' user_var
+read -sp 'password : ' pass_var
+echo
+echo "username is : $user_var"
+echo "password : $pass_var" 
+
+echo "Enter names : "
+read -a names
+echo "Names : ${names[0]}, ${names[1]}"
+
+# if no variable names after read cmd, then the input gets stored in the system variable called REPLY.
+echo "Enter name : "
+read
+echo "Name : $REPLY"
