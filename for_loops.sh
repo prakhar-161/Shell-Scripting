a3f52e258eff2c1606ea6354575ec75308c32542
Prakhar Dhama <pdhama161@gmail.com>
2021-10-17 18:02:46 +0530

    for loops added

---
+++ for_loops.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+# For Loops
+
+# SYNTAXES - 
+
+# for VARIABLE in 1 2 3 4 5 .. N
+# do 
+#     command1
+#     command2
+#     commandN
+# done
+
+# ------------------------------------------------------------------------
+
+# for VARIABLE in file1 file2 file3
+# do
+#     command1
+#     command2
+#     commandN
+# done
+
+# ------------------------------------------------------------------------
+
+# for OUTPUT in $(Linux-Or-Unix-Command-Here)
+# do
+#     command1
+#     command2
+#     commandN
+# done
+
+# ------------------------------------------------------------------------
+
+# for (( EXP1; EXP2; EXP3  ))
+# do
+#     command1
+#     command2
+#     commandN
+# done
+
+# ----------------------------------------------------------------------------------------
+
+# Method - 1
+for i in 1 2 3 4 5 
+do
+    echo $i
+done
+
+# Method - 2
+for (( i=0; i<5; i++ ))
+do 
+    echo $i
+done
