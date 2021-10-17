2d1e2eba12f4d78a3730a63ec3eb0a3d02913bc0
Prakhar Dhama <pdhama161@gmail.com>
2021-10-17 13:50:23 +0530

    updated while loop with sleep cmd

--- while_loops.sh
+++ while_loops.sh
@@ -13,18 +13,31 @@
 # Print numbers from 1 to 10 using while loop.
 
 # Method - 1
-n=1
-while [ $n -le 10 ]
-do
-    echo "$n"
-    # n=$(( n+1 ))
-    (( n++ ))
-done
+# n=1
+# while [ $n -le 10 ]
+# do
+#     echo "$n"
+#     # n=$(( n+1 ))
+#     (( n++ ))
+# done
 
 # Method - 2
-n=1
-while (( $n <= 10 ))
-do 
-    echo "$n"
-    (( n++ ))
-done
+# n=1
+# while (( $n <= 10 ))
+# do 
+#     echo "$n"
+#     (( n++ ))
+# done
+
+# ----------------------------------------------------------------------------------------
+
+# Sleep command in while loops
+# pausing the while loop by the time given in sleep command in seconds
+# n=1
+# while [ $n -le 10 ]
+# do
+#     echo "$n"
+#     (( n++ ))
+#     sleep 1
+# done
+
