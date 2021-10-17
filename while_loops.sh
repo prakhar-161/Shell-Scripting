fdf021f81ccefd1048c18af5e9c6999cde27135d
Prakhar Dhama <pdhama161@gmail.com>
2021-10-17 13:42:15 +0530

    while loop example

---
+++ while_loops.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# While Loops
+
+# SYNTAX - 
+# while [condition]
+# do 
+#     command1
+#     command2
+#     command3
+# done
+
+# Print numbers from 1 to 10 using while loop.
+
+# Method - 1
+n=1
+while [ $n -le 10 ]
+do
+    echo "$n"
+    # n=$(( n+1 ))
+    (( n++ ))
+done
+
+# Method - 2
+n=1
+while (( $n <= 10 ))
+do 
+    echo "$n"
+    (( n++ ))
+done
